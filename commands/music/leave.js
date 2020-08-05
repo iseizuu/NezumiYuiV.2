42f666d555c58629be16dbf52df4339651eb30a9
VeguiIzumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-08-05 19:41:09 +0700

    Fix delete the queue after use leave command

--- commands/music/leave.js
+++ commands/music/leave.js
@@ -8,35 +8,30 @@ module.exports = class LeaveCommand extends Command {
       group: 'music',
       memberName: 'leave',
       guildOnly: true,
-      description: 'Leaves voice channel if in one'
+      description: 'Leaves voice channel'
     });
   }
 
-  run(message) {
-    var voiceChannel = message.member.voice.channel;
-    if (!voiceChannel) return message.reply('You need to join voice channel first');
-    let userVoiceChannel = message.member.voice.channel;
-    if (!userVoiceChannel) {
-      return;
+	run(msg) {
+		const connection = this.client.voice.connections.get(msg.guild.id);
+		if (!connection) return msg.reply('I am not in a voice channel.');
+    if(msg.guild.musicData.isPlaying === true && msg.member.voice.channel.id !== msg.guild.musicData.nowPlaying.voiceChannel.id) {
+      return msg.channel.send({embed : {
+          description: `Error accepting you request, because you not in **${msg.guild.musicData.nowPlaying.voiceChannel.name}** `,
+          color: 'RED'
+      }})
     }
-
-    try { 
-      let clientVoiceConnection = message.guild.voice.connection;
-      if (userVoiceChannel === clientVoiceConnection.channel) {
-      const connection = this.client.voice.connections.get(message.guild.id);
-		  if (!connection) return message.reply('I am not in a voice channel.');
-		  connection.channel.leave();
-		  return message.reply(`Left **${connection.channel.name}**...`);
-      message.guild.musicData.songDispatcher.end();
-      message.guild.musicData.queue.length = 0;
-      return;
-    } else {
-        message.channel.send('You can only execute this command if you share the same voiceChannel!');
-    }
-    } catch (err) {
-			return message.reply(`Im not even in voice channel, did you see?`);
-  }
     
+    if(msg.guild.musicData.isPlaying === true) {
+      msg.guild.musicData.songDispatcher.end();
+      connection.channel.leave();
+      
+    }
+    else {
+      msg.react("✅")
+      connection.channel.leave();
+      return msg.reply(`Left **${connection.channel.name}**...`);
+    }
 
-  }
+	}
 };
