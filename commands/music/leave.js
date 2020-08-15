5e43f39f1d7fe48f1e0239db6a8de510e64c1511
I Seizuu <64743287+VeguiIzumi@users.noreply.github.com>
2020-08-15 14:47:11 +0700

    Wth, how fool i am :x

--- commands/music/leave.js
+++ commands/music/leave.js
@@ -11,31 +11,30 @@ module.exports = class LeaveCommand extends Command {
       description: 'Leaves voice channel'
     });
   }
-
 	run(msg) {
-    if(!msg.member.voice.channel) {
-      return msg.say('Please, join in voice channel')
-    }
-		const connection = this.client.voice.connections.get(msg.guild.id);
-		if (!connection) return msg.reply('I am not in a voice channel.');
-    if(msg.guild.musicData.isPlaying === true && msg.member.voice.channel.id !== msg.guild.musicData.nowPlaying.voiceChannel.id) {
-      return msg.channel.send({embed : {
-          description: `Error accepting you request, because you not in **${msg.guild.musicData.nowPlaying.voiceChannel.name}** `,
-          color: 'RED'
-      }})
-    }
+	    if(!msg.member.voice.channel) {
+	      return msg.say('Please, join in voice channel')
+	    }
+	    const connection = this.client.voice.connections.get(msg.guild.id);
+	    if (!connection) return msg.reply('I am not in a voice channel.');
+	    if(msg.guild.musicData.isPlaying === true && msg.member.voice.channel.id !== msg.guild.musicData.nowPlaying.voiceChannel.id) {
+	      return msg.channel.send({embed : {
+		  description: `Error accepting you request, because you not in **${msg.guild.musicData.nowPlaying.voiceChannel.name}** `,
+		  color: 'RED'
+	      }})
+	    }
 
-    msg.guild.musicData.songDispatcher.resume() //handling when user paused the queue
-    if(msg.guild.musicData.isPlaying === true) {
-      msg.guild.musicData.songDispatcher.end();
-      msg.guild.musicData.queue.length = 0;
-      
-    }
-    else {
-      msg.react("✅")
-      connection.channel.leave();
-      return msg.reply(`Left **${connection.channel.name}**...`);
-    }
+	    
+	    if(msg.guild.musicData.isPlaying === true) {
+	      msg.guild.musicData.songDispatcher.resume() //handling when user paused the queue
+	      msg.guild.musicData.songDispatcher.end();
+	      msg.guild.musicData.queue.length = 0;
 
+	    }
+	    else {
+	      msg.react("✅")
+	      connection.channel.leave();
+	      return msg.reply(`Left **${connection.channel.name}**...`);
+	    }
 	}
 };
