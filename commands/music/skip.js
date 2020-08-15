5623532cdf053472808117fd8a02d0fd82b99aa2
I Seizuu <64743287+VeguiIzumi@users.noreply.github.com>
2020-08-15 19:45:38 +0700

    Update skip.js

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -13,40 +13,36 @@ module.exports = class SkipCommand extends Command {
   }
 
   run(message) {
-    
-    
+
     const voiceChannel = message.member.voice.channel;
     if (!voiceChannel) return message.reply('You need to join voice channel first');
       if (
       typeof message.guild.musicData.songDispatcher == 'undefined' ||
-      message.guild.musicData.songDispatcher == null
-    ) {
-      return message.reply('There is no song playing right now!');
+      message.guild.musicData.songDispatcher == null) {
+        return message.reply('There is no song playing right now!');
       }   
         
-  if(!message.guild.voice.connection)
-  {
-    return;
+    if(!message.guild.voice.connection) {
+      return;
+    }
+    let userVoiceChannel = message.member.voice.channel;
+    if (!userVoiceChannel) {
+      return;
+    }
+    let clientVoiceConnection = message.guild.voice.connection;
+    if (userVoiceChannel === clientVoiceConnection.channel) {
+      message.guild.musicData.songDispatcher.resume()
+      message.guild.musicData.songDispatcher.end(); 
+      const embed = new MessageEmbed()
+      .setColor('#cce7e8')
+      .setAuthor('Skip' ,'https://cdn.discordapp.com/attachments/688763072864976906/706472099082141696/661493093811617803.gif')
+      .addField('✔ | Successfully skipped', `**${message.guild.musicData.nowPlaying.title}**`)
+      .setTimestamp()
+      .setFooter(`Skipped by ${message.author.username}`)
+      message.say(embed);
+    } else {
+      message.channel.send('You can only execute this command if you share the same voiceChannel!');
+     }
+         
   }
-  let userVoiceChannel = message.member.voice.channel;
-  if (!userVoiceChannel) {
-    return;
-  }
-  let clientVoiceConnection = message.guild.voice.connection;
-  if (userVoiceChannel === clientVoiceConnection.channel) {
-    message.guild.musicData.songDispatcher.end(); 
-    const embed = new MessageEmbed()
-    .setColor('#cce7e8')
-    .setAuthor('Skip' ,'https://cdn.discordapp.com/attachments/688763072864976906/706472099082141696/661493093811617803.gif')
-    .addField('✔ | Successfully skipped', `**${message.guild.musicData.nowPlaying.title}**`)
-    .setTimestamp()
-    .setFooter(`Skipped by ${message.author.username}`)
-    message.say(embed);
-  } else {
-    message.channel.send('You can only execute this command if you share the same voiceChannel!');
-  }
-     
-
-    
-}
 };
